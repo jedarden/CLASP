2909b430e806452efbad412784943d56f2810029
jed-arden <jed@jedarden.com>
2025-12-07 10:42:55 +0000

    chore: Sync source version to v0.28.18 to match published npm release

--- cmd/clasp/main.go
+++ cmd/clasp/main.go
@@ -24,7 +24,7 @@ import (
 )
 
 var (
-	version = "v0.28.17"
+	version = "v0.28.18"
 )
 
 func main() {
--- scripts/install.js
+++ scripts/install.js
@@ -12,7 +12,7 @@ const path = require('path');
 const { execSync } = require('child_process');
 const os = require('os');
 
-const VERSION = '0.28.17';
+const VERSION = '0.28.18';
 const REPO = 'jedarden/CLASP';
 const BINARY_NAME = 'clasp';
 
